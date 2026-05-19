479079b205b385950dbcdf1f0ef79feecac4e55a
m-u-hasan <hasan.cse.duet@gmail.com>
2026-05-20 00:16:44 +0600

    7-3 Setting Up Postgres with Neon Serverless Cloud

--- Module-7/src/server.ts
+++ Module-7/src/server.ts
@@ -1,33 +1,46 @@
 import express, {type Application, type Request, type Response} from "express";
+import{Pool} from "pg";
 //const express = require('express')
 const app : Application = express();
 const port = 3000;
+app.use(express.json());// MiddleWare
 
-app.use(express.json());// Middle Ware
 
+
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
 
 app.get('/', (req : Request, res:Response) => {
   res.send('Hello World! This is express Server');
-})
+});
 
 app.get('/User', (req: Request , res: Response) => {
  // res.send('This is user route');
   res.status(200).json({
     "message" : "Express Server",
     "author": "Next",
-  })
+  });
 
-})
+});
 
 app.post('/', async(req: Request, res: Response)=>
 {
-    console.log(req.body);
+    //console.log(req.body);
+
+    const {name, email, password}=req.body;
 
     res.status(201).json({
         "success": true,
-        data: req.body,
-    })
-})
+        "message": "Created Data",
+        data: {
+            name,
+            email,
+        },
+    });
+});
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
