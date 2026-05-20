233db67410c8b8a5a32099a80af088f48b0d91c8
m-u-hasan <hasan.cse.duet@gmail.com>
2026-05-21 00:40:13 +0600

    Completed: 7-5 Executing Pool and creating tables

--- Module-7/src/server.ts
+++ Module-7/src/server.ts
@@ -1,83 +1,49 @@
-import express, {type Application, type Request, type Response} from "express";
-import { timeStamp } from "node:console";
-import{Pool} from "pg";
-//const express = require('express')
-const app : Application = express();
-const port = 3000;
-app.use(express.json());// MiddleWare
+import express, { type Application, type Request, type Response } from "express";
+import { Pool } from "pg";
+import dotenv from "dotenv";
 
+dotenv.config();
 
+const app: Application = express();
 
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
-app.get('/', (req : Request, res:Response) => {
-  res.send('Hello World! This is express Server');
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World! This is express Server");
 });
 
-app.get('/User', (req: Request , res: Response) => {
- // res.send('This is user route');
-  res.status(200).json({
-    "message" : "Express Server",
-    "author": "Next",
-  });
-
-});
-
-//=================Data Type of SQL===============
-        //boolean
-        //Numbers
-        //Binary
-        //Date/time
-        //Json
-        //Character
-        //UUID
-        //Array
-        //XML
-//=================Data Type of SQL==============
-
-
-const initDB=async()=>{
-  try{
+const initDB = async () => {
+  try {
     await pool.query(`
-        CREATED TABLE IF NOT EXIST users (
-          id SERIAL PRIMARY KeyboardEvent,
-          name VARCHAR(20),
-          email VARCHAR(20) NOT NULL,
-          password VARCHAR(20) NOT null,
-          age int,
-          is_active BOOLEAN DEFAULT true,
-          
-          created_at TIMESTAMP DEFAULT New(),
-          updated_at TIMESTAMP DEFAULT now()
+      CREATE TABLE IF NOT EXISTS users (
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(50),
+        email VARCHAR(255) NOT NULL,
+        password TEXT NOT NULL,
+        age INT,
+        is_active BOOLEAN DEFAULT true,
+
+        created_at TIMESTAMP DEFAULT now(),
+        updated_at TIMESTAMP DEFAULT now()
       )
-      `)
-      console.log("Database Connected");
-  }catch (error)
-  {
-    console.log(error);
+    `);
+
+    console.log("Database Connected");
+  } catch (error) {
+    console.error("DB ERROR:", error);
   }
 };
+
 initDB();
-
-
-app.post('/', async(req: Request, res: Response)=>
-{
-    //console.log(req.body);
-
-    const {name, email, password}=req.body;
-
-    res.status(201).json({
-        "success": true,
-        "message": "Created Data",
-        data: {
-            name,
-            email,
-        },
-    });
-});
 
 
 app.listen(port, () => {
