dd0b86663f07ce38b6c9e17f06b0e733d3aa5c5f
m-u-hasan <hasan.cse.duet@gmail.com>
2026-05-21 01:38:14 +0600

    7-6 Creating our first User with POST method

--- Module-7/src/server.ts
+++ Module-7/src/server.ts
@@ -4,7 +4,7 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const app: Application = express();
+const app: Application = express()
 
 const port = process.env.PORT || 3000;
 
@@ -21,13 +21,15 @@ app.get("/", (req: Request, res: Response) => {
   res.send("Hello World! This is express Server");
 });
 
+
+//=================Created Table===================
 const initDB = async () => {
   try {
     await pool.query(`
       CREATE TABLE IF NOT EXISTS users (
         id SERIAL PRIMARY KEY,
         name VARCHAR(50),
-        email VARCHAR(255) NOT NULL,
+        email VARCHAR(255) UNIQUE NOT NULL,
         password TEXT NOT NULL,
         age INT,
         is_active BOOLEAN DEFAULT true,
@@ -45,7 +47,44 @@ const initDB = async () => {
 
 initDB();
 
+app.get('/', (req: Request,res:Response)=>
+{
+  res.status(201).json(
+    {
+      message: "Express Server",
+      author: "Next Level"
+    }
+  );
+});
+
+
+//==============Insert Data in USER table=================
+app.post('/', async(req:Request, res:Response)=>
+{
+const {name, email, password, age}=req.body;
+try{
+const result = await pool.query(`
+  INSERT INTO users(name, email, password, age) VALUES($1,$2,$3,$4)
+  RETURNING *`, [name, email, password, age]);
+  console.log(result);
+
+  res.status(201).json(
+    {
+      message: "User create Successfully",
+      data:result.rows[0],
+    }
+  )
+}catch (error:any) {
+ res.status(500).json(
+    {
+      message: error.message,
+      error: error,
+    }
+  )
+}
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
-})
+});
+
