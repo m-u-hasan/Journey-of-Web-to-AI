7c601ba2f685ff61244dab81815d88be2ad3786b
m-u-hasan <hasan.cse.duet@gmail.com>
2026-05-25 23:45:08 +0600

    create auth.service for createUser

---
+++ 01. ConceptualSession/Mission-2-2/src/api/routes/auth.routes.ts
@@ -0,0 +1,12 @@
+import {Router} from "express"
+
+
+const router =Router()
+
+router.post ("/signup", ()=>{})
+router.post ("/login", ()=>{})
+
+router.get ("/me", ()=>{})
+
+router.put ("/update/:id", ()=>{})
+router.delete ("/delete/:id", ()=>{})
