13460a61142ebfb9c4863cbda8bbe56e227b6378
m-u-hasan <hasan.cse.duet@gmail.com>
2026-07-22 21:51:27 +0600

    Connection with prisma DB successfull

--- Module-20/src/server.ts
+++ Module-20/src/server.ts
@@ -1,15 +1,19 @@
 import app from "./app"
+import { prisma } from "./lib/prisma";
 
 const PORT=process.env.PORT || 5000;
 
 async function main() {
     try{
+        await prisma.$connect();
+        console.log("Connected with Prisma Database successfully.");
         app.listen(PORT, ()=>{
             console.log(`Server is Running on port ${PORT}`);
         });
 
     }catch(error){
         console.error("Error starting the server: ", error);
+        await prisma.$disconnect();
         process.exit(1);
 
     }    
