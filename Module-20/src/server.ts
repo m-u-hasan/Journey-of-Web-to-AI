3da50322ae8db5ee1f41a97c92defb63593f62bf
m-u-hasan <hasan.cse.duet@gmail.com>
2026-07-24 09:54:17 +0600

    Express setup completed

--- Module-20/src/server.ts
+++ Module-20/src/server.ts
@@ -1,22 +1,23 @@
 import app from "./app"
+import "dotenv/config";
 import { prisma } from "./lib/prisma";
 
-const PORT=process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
 async function main() {
-    try{
-        await prisma.$connect();
+    try {
+        //await prisma.$connect();
         console.log("Connected with Prisma Database successfully.");
-        app.listen(PORT, ()=>{
+        app.listen(PORT, () => {
             console.log(`Server is Running on port ${PORT}`);
         });
 
-    }catch(error){
+    } catch (error) {
         console.error("Error starting the server: ", error);
-        await prisma.$disconnect();
+        //await prisma.$disconnect();
         process.exit(1);
 
-    }    
+    }
 }
 
 main()
