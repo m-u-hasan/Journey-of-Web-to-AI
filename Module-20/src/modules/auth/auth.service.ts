01de833af22207a1736244f2f31ee490000ac6f6
m-u-hasan <hasan.cse.duet@gmail.com>
2026-09-08 23:01:23 +0600

    21-4: access token & refresh token

--- Module-20/src/modules/auth/auth.service.ts
+++ Module-20/src/modules/auth/auth.service.ts
@@ -1,20 +1,49 @@
+import config from "../../config";
 import { prisma } from "../../lib/prisma";
 import { ILoginUser } from "./auth.interface";
 import bcrypt from "bcrypt";
+import jwt, { SignOptions } from "jsonwebtoken";
 
-const loginUser = async(payload: ILoginUser) => { 
-    const {email, password}=payload;
+const loginUser = async (payload: ILoginUser) => {
+    const { email, password } = payload;
 
     const user = await prisma.user.findFirstOrThrow({
-        where: {email}
+        where: { email }
     })
-
     const isPasswordMatched = await bcrypt.compare(password, user.password);
-
-    if(!isPasswordMatched){
-        throw new Error ("Password is incorrect");
+    if (!isPasswordMatched) {
+        throw new Error("Password is incorrect");
     }
-    return user;
+
+
+    const jwtPayload = {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        role: user.role
+    }
+
+
+
+    const accessToken = jwt.sign(jwtPayload, config.jwt_access_secret, {
+        expiresIn: config.jwt_access_expires_in
+    } as SignOptions
+    )
+
+    const refreshToken = jwt.sign(
+        jwtPayload, 
+        config.jwt_refresh_seccret,
+         { expiresIn: config.jwt_refresh_expires_in 
+            
+         } as SignOptions
+    );
+
+
+
+    return {
+        accessToken,
+        refreshToken
+    };
 
 }
 
