a79d56567601915f02193c81caae58ba7fab6e59
m-u-hasan <hasan.cse.duet@gmail.com>
2026-09-12 10:48:53 +0600

    21-8 Optimizing The Auth Middleware With Higher Order Function

--- Module-21/src/modules/users/user.route.ts
+++ Module-21/src/modules/users/user.route.ts
@@ -6,13 +6,15 @@ import bcrypt from "bcrypt";
 import { userController } from "./user.controller";
 import { jwtUtils } from "../../utils/jwt";
 import { Role } from "../../../generated/prisma/enums";
+import { catchAsync } from "../../utils/catchAsync";
+import { JwtPayload } from "jsonwebtoken";
 
 const router = Router();
 
-declare global{
-    namespace Express{
-        interface Request{
-            user?:{
+declare global {
+    namespace Express {
+        interface Request {
+            user?: {
                 email: string;
                 name: string;
                 id: string;
@@ -24,38 +26,105 @@ declare global{
 
 router.post("/register", userController.registerUser)
 
-router.get("/me", (req: Request, res: Response, next: NextFunction) => {
-    console.log(req.cookies);
+//auth(Role.Admin, Role.User, Role.Author)
+//auth()=> .....requiredRoles => [Role.ADMIN, Role.USER, Role.AUTHOR]
 
-    //from user controller
-    const { accessToken } = req.cookies;
-    console.log(accessToken);
+const auth = (...requiredRoles:Role[]) => {
 
-    const verifiedToken = jwtUtils.verifyToken(accessToken, config.jwt_access_secret);
+    return catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+        const token = req.cookies.accessToken || (req.headers.authorization?.startsWith("Bearer ") ? req.headers.
+            authorization?.split(" ")[1]
+            : req.headers.authorization);
 
-    if (typeof verifiedToken === "string") {
-        throw new Error(verifiedToken);
-    }
-    const { email, name, id, role } = verifiedToken;
-    const requiredRoles = [Role.ADMIN, Role.USER, Role.AUTHOR];
 
-    if (!requiredRoles.includes(role)) {
-        return res.status(403).json({
-            success: false,
-            statusCode: httpStatus.FORBIDDEN,
-            message: "Forbidden. You don't have permission to access this resource. "
+        if (!token) {
+            throw new Error("You are note logged in. Please log in to access this resource.")
+        }
+
+        const verifiedToken = jwtUtils.verifyToken(token, config.jwt_access_secret);
+
+        if (!verifiedToken.success) {
+            throw new Error(verifiedToken.error);
+
+        }
+        const { email, name, id, role } = verifiedToken.data as JwtPayload;
+
+        if(requiredRoles.length && !requiredRoles.includes(role)){
+            throw new Error ("Forbidden. You don't have permission to acces the resource.");
+        }
+
+
+        const user = await prisma.user.findUnique({
+            where: {
+                id,
+                email,
+                name, 
+                role
+            }
         })
-    }
+        if (!user){
+            throw new Error ("Usre not found, please log in again")
+        }
 
-req.user={
-    email,
-    name,
-    id,
-    role
-};
+        if(user.activeStatus=== "BLOCKED"){
+            throw new Error ("Your Account has been blocked, Please contac support.")
+        }
 
-    next();
-}, userController.getMyProfile)
+        req.user ={
+            email,
+            name,
+            id,
+            role
+        }
+        next();
+
+    })
+}
+
+
+
+router.get("/me",
+    
+//     (req: Request, res: Response, next: NextFunction) => {
+//     console.log(req.cookies);
+
+//     //from user controller
+//     const { accessToken } = req.cookies;
+//     console.log(accessToken);
+
+//     const verifiedToken = jwtUtils.verifyToken(accessToken, config.jwt_access_secret);
+
+//     if (!verifiedToken.success) {
+//         throw new Error(verifiedToken.error);
+
+//     }
+
+//     // if (typeof verifiedToken === "string") {
+//     //     throw new Error(verifiedToken);
+//     // }
+//     const { email, name, id, role } = verifiedToken.data as JwtPayload;
+
+//     const requiredRoles = [Role.ADMIN, Role.USER, Role.AUTHOR];
+
+//     if (!requiredRoles.includes(role)) {
+//         return res.status(403).json({
+//             success: false,
+//             statusCode: httpStatus.FORBIDDEN,
+//             message: "Forbidden. You don't have permission to access this resource. "
+//         })
+//     }
+
+//     req.user = {
+//         email,
+//         name,
+//         id,
+//         role
+//     };
+
+//     next();
+// },
+auth(Role.ADMIN, Role.USER, Role.AUTHOR),
+userController.getMyProfile)
 
 
 
